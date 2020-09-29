696b756a2e4c3e7d6b2ce579c9c6ef332fa80359
Nicolas Bouvard <nicolas.bouvard16@gmail.com>
2020-09-29 12:06:05 +0200

    Create liste = [].py

---
+++ liste = [].py
@@ -0,0 +1,12 @@
+liste = [] 
+def nombre () :
+    for i in range(999):
+        number = f'{i:03}'
+        l = list(number)
+        if number not in liste
+            liste.append(number)
+            if l[0] != l[1] and l[1] != l[2] and l[2] != l[0] :
+                print(number)   
+
+nombre()
+    
